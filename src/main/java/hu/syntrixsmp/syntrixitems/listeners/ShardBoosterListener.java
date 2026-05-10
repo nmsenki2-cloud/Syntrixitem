dbd63e98df4c9c521df4742723b1ee9c36156e31
nmsenki2-cloud <nmsenki2@gmail.com>
2026-05-10 10:58:45 +0200

    Create ShardBoosterListener.java

---
+++ src/main/java/hu/syntrixsmp/syntrixitems/listeners/ShardBoosterListener.java
@@ -0,0 +1,64 @@
+package hu.syntrixsmp.syntrixitems.listeners;
+
+import hu.syntrixsmp.syntrixitems.SyntrixItemsPlugin;
+import hu.syntrixsmp.syntrixitems.managers.BoosterManager;
+import hu.syntrixsmp.syntrixitems.utils.ItemUtils;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class ShardBoosterListener implements Listener {
+    private final SyntrixItemsPlugin plugin;
+
+    public ShardBoosterListener(SyntrixItemsPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK) return;
+
+        Player player = event.getPlayer();
+        ItemStack item = player.getInventory().getItemInMainHand();
+
+        if (!ItemUtils.isCustomItem(item, ItemUtils.SHARD_BOOSTER_KEY)) return;
+
+        event.setCancelled(true);
+
+        BoosterManager manager = plugin.getBoosterManager();
+
+        if (manager.hasActiveBooster(player)) {
+            player.sendMessage(c("&c&l[Booster] &cMár van aktív boostere! Hátralévő: &e" + manager.getFormattedRemaining(player)));
+            return;
+        }
+
+        manager.activateBooster(player);
+
+        if (item.getAmount() > 1) item.setAmount(item.getAmount() - 1);
+        else player.getInventory().setItemInMainHand(null);
+
+        int hours = plugin.getConfig().getInt("shard-booster.duration-seconds", 86400) / 3600;
+        int pps = plugin.getConfig().getInt("shard-booster.points-per-second", 4);
+
+        player.sendMessage(c("&b&l[Booster] &bShard Booster aktiválva! &7" + pps + " Shard/mp " + hours + " óráig!"));
+
+        player.showTitle(net.kyori.adventure.title.Title.title(
+            c("&b&lShard Booster"),
+            c("&7" + pps + " Shard/mp - " + hours + " óra"),
+            net.kyori.adventure.title.Title.Times.times(
+                java.time.Duration.ofMillis(500),
+                java.time.Duration.ofMillis(3000),
+                java.time.Duration.ofMillis(1000)
+            )
+        ));
+    }
+
+    private Component c(String text) {
+        return LegacyComponentSerializer.legacyAmpersand().deserialize(text);
+    }
+}
